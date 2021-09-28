8e822a81bc9ba56bc75f96f0e0e9fac0d9ae8ad8
Aiden <aiden8green@gmail.com>
2021-09-28 13:21:05 -0400

    requests session for SPEED

--- oncourse_api/oncourse.py
+++ oncourse_api/oncourse.py
@@ -1,44 +1,48 @@
 from json import loads
 import requests
-from re import MULTILINE, DOTALL, search
+from re import MULTILINE, search
 
 
 class OnCourse:
     def __init__(self, username: str, password: str):
-        self.username:str = username
+        self.requestSession = requests.Session()
+        """ Start the request session """
+        self.username: str = username
         """ Login Username """
         self.password = password
         """ Login Password """
         self.cookie: str = self.getCookie()
         """ Returns OnCourse auth cookie """
-        self.activeProfile: dict = self.activeProfile()
+        self.active_profile: dict = self.activeProfile()
         """ Returns OnCourse active user Info """
         self.id: int = self.activeProfile["id"]
         """ Returns OnCourse User ID """
         self.name: int = self.activeProfile["name"]
         """ Returns Student Name """
-        self.schoolId: int = self.activeProfile["schoolId"]
+        self.school_id: int = self.activeProfile["schoolId"]
         """ Returns School ID """
-        self.schoolYearId: int = self.activeProfile["schoolYearId"]
+        self.school_year_id: int = self.activeProfile["schoolYearId"]
         """ Returns School Year ID """
-        self.schoolName: str = self.activeProfile["schoolName"]
+        self.school_name: str = self.activeProfile["schoolName"]
         """ Returns School Name """
-        self.reportCard: str = self.reportCard()
+        self.report_card: str = self.reportCard()
         """ Returns Report Card """
+        self.studen_potriat = f"https://www.oncourseconnect.com/json.axd/file/image?app=STUDENT_PORTRAITS&id={self.id}"
+        
 
-    def getCookie(self) -> str:
+    def getCookie(self) -> bool:
+        print("Getting Cookie")
         url = "https://www.oncourseconnect.com/account/login"
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
         data = {"Username": self.username, "Password": self.password}
-        session = requests.Session()
-        session.post(url, headers=headers, data=data)
-        oncourse_cookie = session.cookies["_occauth"]
+        self.requestSession.post(url, headers=headers, data=data)
+        oncourse_cookie = self.requestSession.cookies["_occauth"]
+        self.requestSession.headers.update({"Cookie": f"_occauth={oncourse_cookie}"})
         return oncourse_cookie
 
     def activeProfile(self) -> int:
         url = "https://www.oncourseconnect.com/#/studentdata"
-        headers = {"Cookie": f"_occauth={self.cookie}"}
-        source = (requests.get(url, headers=headers)).text
+        source = (self.requestSession.get(url)).text
         regex = r"window.activeProfile = {(.*)}"
         windowActiveProfile = search(regex, source, MULTILINE)
         activeProfile = "{" + windowActiveProfile.group(1) + "}"
@@ -46,6 +50,6 @@ class OnCourse:
 
     def reportCard(self) -> dict:
         url = f"https://www.oncourseconnect.com/api/classroom/grades/report_cards?schoolId={self.schoolId}&schoolYearId={self.schoolYearId}&studentId={self.id}"
-        headers = {"Cookie": f"_occauth={self.cookie}"}
-        report_card = requests.get(url, headers=headers)
+        report_card = self.requestSession.get(url)
         return report_card.json()
+
