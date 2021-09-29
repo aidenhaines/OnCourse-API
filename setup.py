ff70f8e830173b8c76b8a9972c689b17795f0931
Aiden <aiden8green@gmail.com>
2021-09-29 16:41:30 -0400

    Update setup.py
    I dont know why pypi thinks dupe file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md","r",encoding="utf-8") as f:
 
 setup(
     name="oncourse_api",
-    version="1.1.0",
+    version="1.1.1",
     description="A python api wrapper for OnCourse Connect",
     long_description=long_description,
     long_description_content_type="text/markdown",
