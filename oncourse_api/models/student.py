ed5827c80f8e648eacc310674c71e4a85a6acbf0
Aiden <aiden8green@gmail.com>
2021-10-09 00:19:16 -0400

    moved to models

--- oncourse_api/models/student.py
+++ oncourse_api/models/student.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Student:
     """ Make the orginazation better for student """
     def __init__(self, student: dict, school_id: int, school_year_id: int, request_session):
@@ -16,6 +18,9 @@ class Student:
         self.school_id = school_id
         self.school_year_id = school_year_id
         self.requestSession = request_session
+        self.classes = self.getClasses()
+        self.assignments = self.getAssignments()
+        
 
         self.studen_potriat = f"https://www.oncourseconnect.com/json.axd/file/image?app=STUDENT_PORTRAITS&id={self.id}"
         self.reportCard = self.reportCard()
@@ -23,13 +28,26 @@ class Student:
 
     def reportCard(self) -> dict:
         url = f"https://www.oncourseconnect.com/api/classroom/grades/report_cards?schoolId={self.school_id}&schoolYearId={self.school_year_id}&studentId={self.id}"
-        report_card = self.requestSession.get(url)
-        return report_card.json()
+        report_card = (self.requestSession.get(url)).json()
+        return report_card
 
     def getAttendence(self) -> dict:
         url = f"https://www.oncourseconnect.com/api/classroom/attendance/attendance_summary?schoolId={self.school_id}&schoolYearId={self.school_year_id}&studentId={self.id}"
         attendance = (self.requestSession.get(url)).json()
         return attendance
+
+    def getClasses(self) -> dict:
+        url = f"https://www.oncourseconnect.com/json.axd/classroom/lms/classes/list_student_groups?showAll=N&studentId={self.id}"
+        classes = (self.requestSession.get(url)).json()
+        return classes
+
+    def getAssignments(self) -> dict:
+        today = date.today()
+        startDate:str = today.strftime("%m/%d/%Y")
+        endDate:str = str(today.month) + "/" + str(today.day) + "/" + str(today.year + 1)
+        url = f"https://www.oncourseconnect.com/json.axd/classroom/lms/assignments/get_student_work_due?endDate={endDate}&startDate={startDate}&studentId={self.id}"
+        assignments = (self.requestSession.get(url)).json()
+        return assignments
 
 
 
