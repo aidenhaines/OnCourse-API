c3e5121a9f06346f755b7bccf62bc9d81071fc6c
Aiden <aiden8green@gmail.com>
2021-10-30 19:51:03 -0400

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md","r",encoding="utf-8") as f:
 
 setup(
     name="oncourse_api",
-    version="2.0.0",
+    version="2.0.1",
     description="A python api wrapper for OnCourse Connect",
     long_description=long_description,
     long_description_content_type="text/markdown",
