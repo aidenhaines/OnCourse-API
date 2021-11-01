0c02cdb7881c3acf7c1509aee8d336951ffdfeed
Aiden <aiden8green@gmail.com>
2021-11-01 15:09:40 -0400

    Read comment

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
-from distutils.core import setup
-from setuptools import find_packages
+from pathlib import Path
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+import tomli
+from setuptools import find_packages, setup
+
+with open("pyproject.toml", "rb") as f:
+    pyproject = tomli.load(f)
 
 setup(
-    name="oncourse_api",
-    version="2.1.1",
-    description="A python api wrapper for OnCourse Connect",
-    long_description=long_description,
+    name=pyproject["tool"]["poetry"]["name"],
+    version=pyproject["tool"]["poetry"]["version"],
+    description=pyproject["tool"]["poetry"]["description"],
+    long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
     url="https://github.com/Wolfhound905/OnCourse-API",
     author="Wolfhound905",
     author_email="aiden8green@gmail.com",
     license="MIT",
-    packages=find_packages(
-        include=["oncourse_api.*", "oncourse_api"], exclude=["test", "tests", "testing"]
-    ),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["requests"],
+    install_requires=(Path(__file__).parent / "requirements.txt").read_text().splitlines(),
 )
